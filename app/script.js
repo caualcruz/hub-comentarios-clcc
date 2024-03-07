99b777eb9a8b289ac55fb8aa2fab81efb8a65aa4
caualcruz <caualucas34492@gmail.com>
2024-03-07 20:48:15 -0300

    adicionando data aos comentarios

--- app/script.js
+++ app/script.js
@@ -1,36 +1,61 @@
-let data = [];
+let data = []
 
-const submitComment = () => {
-  event.preventDefault();
+const submitComment = (event) => {
+    event.preventDefault();
 
-  const author = inputAuthor.value;
-  const comment = inputComment.value;
+    const author = inputAuthor.value;
+    const comment = inputComment.value;
+    const date = horaComent();
 
-  data.push({ author: author, comment: comment });
+    data.push({ author: author, comment: comment })
+    console.log(data)
 
-  //Implementar a chamada do endpoint da api para salvar os dados no banco
-  loadComment();
-};
+    loadComment()
+}
 
-const formComentario = document.getElementById("formComment");
-formComentario.addEventListener("submit", submitComment);
+const formComentario = document.getElementById('formComment')
+formComentario.addEventListener("submit", submitComment)
 
 const loadComment = () => {
-    //Dados carregados da API
-
+    // Dados carregados da API
     if (data) {
-        displayComment()
+        displayComment();
     }
-};
+}
+
+const horaComent = () =>{
+    const dataAtual = new Date();
+    const options = { day:'numeric', month:'short', hour: 'numeric'};
+    const date = `Dia ${dataAtual.toLocaleString('pt-BR', options)} horas`
+    return date
+}
 
 const displayComment = () => {
-  const body = document.getElementsByTagName('body')[0];
-  data.forEach(elemento => {
-    const divDisplay = document.createElement('div');
-    divDisplay.className = 'comentarios';
-    divDisplay.innerHTML = ` <strong>${elemento.author}</strong>
-  <p>${elemento.comment}</p>`;
+    const divFeed = document.getElementById('comment-feed');
+    divFeed.innerHTML = '';   
 
-    body.appendChild(divDisplay);
-  });
-};
+    data.forEach(item => {
+        const divDisplay = document.createElement('div');
+        divDisplay.className = 'd-flex text-body-secondary pt-3'
+        divDisplay.innerHTML += `
+        <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+            xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+            preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Comentário</title>
+            <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
+                dy=".3em">32x32</text>
+        </svg>
+        <p class="pb-3 mb-0 small lh-sm border-bottom" id="showComment">
+            <strong class="d-block text-gray-dark">@${item.author}
+             | ${horaComent()}
+            </strong>
+
+            ${item.comment}
+        </p>
+
+        `
+        divFeed.appendChild(divDisplay);
+    })
+
+
+}
